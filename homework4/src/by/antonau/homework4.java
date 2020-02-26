a9e44405702dade8bc7ed2854decb917cc11e701
KirylLog <kirik.by.ru@gmail.com>
2020-02-26 19:56:33 +0300

    Task 4.

---
+++ homework4/src/by/antonau/homework4.java
@@ -0,0 +1,15 @@
+package by.antonau;
+
+public class homework4 {
+
+    public static void main(String[] args) {
+        int a = 16;
+        int b = 11;
+        int r = 12;
+        if (a < r)
+            if (b < r)
+                System.out.println("Круг закроет прямоугольное отверстие");
+        else
+                System.out.println("Круг не закроет прямоугольное отверстие");
+    }
+}
