b8ded58309ac7e1f56c88a40c437c09205278c2f
KirylLog <kirik.by.ru@gmail.com>
2020-02-28 20:16:58 +0300

    six commit

---
+++ homework8/src/by/antonau/homework8.java
@@ -0,0 +1,14 @@
+package by.antonau;
+
+public class homework8 {
+	public static void main(String[] args) {
+		int factorial = 1;
+		int i = 1;
+		while (i <= 10) {
+			factorial *= i;
+			i++;
+		}
+		System.out.println("Factorial of the number 200 = "+factorial);
+	}
+}
+
