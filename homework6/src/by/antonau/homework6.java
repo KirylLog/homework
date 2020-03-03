e52063047bbdb0956f8f5becb1df5525fb61bdf2
KirylLog <kirik.by.ru@gmail.com>
2020-03-03 17:00:54 +0300

    nine

--- homework6/src/by/antonau/homework6.java
+++ homework6/src/by/antonau/homework6.java
@@ -2,28 +2,79 @@ package by.antonau;
 
 public class homework6 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int day = 2;
-		int mount = 2;
-		int year = 2020;
-		int i = 1;
+        int day = 1;
+        int mount = 1;
+        int year = 2020;
+        int i = 1;
 
-		while (mount == 2) {
-			if (day > 30) {
-				mount += i;
-				day = 1;
-			} else {
-				day += i;
+        while (year >= 2020 & mount <= 12 & year < 2030) { // ограничил до 2030
+            if (mount == 1) {
+                while (day < 31) {
+                    day += i;
+                    System.out.println(day + "." + mount + "." + year + ".");
+                }
+                mount += i;
+                day = 1;
+                System.out.println(day + "." + mount + "." + year + ".");
+            } else if (mount == 2 && year % 4 == 0) { //условие на високосный год
+                while (day < 29) {
+                    day += i;
+                    System.out.println(day + "." + mount + "." + year + ".");
+                }
+                mount += i;
+                day = 1;
+                System.out.println(day + "." + mount + "." + year + ".");
 
-	    System.out.println(day + "." + mount + "." + year + ".");
+            } else if (mount == 2 && year % 4 != 0) { //условие на не високосный год
+                while (day < 28) {
+                    day += i;
+                    System.out.println(day + "." + mount + "." + year + ".");
+                }
+                mount += i;
+                day = 1;
+                System.out.println(day + "." + mount + "." + year + ".");
 
-
-			}
-		}
-
-	}
+            } else if (mount != 1 & mount != 2 & mount % 2 != 0) { // подсчёт дат нечётных месяцев
+                while (day < 30) {
+                    day += i;
+                    System.out.println(day + "." + mount + "." + year + ".");
+                }
+                mount += i;
+                day = 1;
+                System.out.println(day + "." + mount + "." + year + ".");
+            } else if (mount != 1 & mount != 2 & mount % 2 == 0) { // подсчёт дат чётных месяцев
+                while (day < 31 & mount <= 12) {
+                    day += i;
+                    System.out.println(day + "." + mount + "." + year + ".");
+                }
+                mount += i;
+                day = 1;
+                if (mount <= 12) {  // ограничение 12-ю месяцами
+                    System.out.println(day + "." + mount + "." + year + ".");
+                }else {
+                    year += 1;
+                    mount = 1;
+                }
+            }
+        }
+        System.out.println("date is not relevant"); // сообщение о выходе за диапазон
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ homework9/src/by/antonau/Homework9.java
@@ -0,0 +1,22 @@
+package by.antonau;
+
+public class Homework9 {
+
+    public static void main(String[] args) {
+	double sr = 0;
+    double sum = 0;
+    int n = 0;
+    int x = (int) (Math.random()*20);
+    while(x !=0) {
+        sum += x;
+        n++;
+        x = (int) (Math.random() * 20);
+    }
+    if(n!=0) {
+        sr = sum / n;
+    } else {
+        sr = 0;
+    }
+        System.out.println("Среднее:" +sr);
+    }
+}
