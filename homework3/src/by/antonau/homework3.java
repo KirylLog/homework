6615a67099a92dbb2320e0b23e573e6a1e2cd32f
KirylLog <kirik.by.ru@gmail.com>
2020-03-04 19:53:43 +0300

    one commit

---
+++ homework3/src/by/antonau/homework3.java
@@ -0,0 +1,16 @@
+package by.antonau;
+
+public class homework3 {
+
+
+        public static void main(String[] args) {
+            double x = 356;
+            double a = x % 10;
+            if(a == 7)
+                System.out.println("Последняя цифра в числе равна 7");
+            else
+                System.out.println("Последняя цифра в числе не равна 7");
+
+        }
+    }
+
