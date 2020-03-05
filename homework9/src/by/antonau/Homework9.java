36e444a05c4a02d388c93305225b807231f90ce7
KirylLog <kirik.by.ru@gmail.com>
2020-03-05 11:18:45 +0300

    eleven commit

--- homework9/src/by/antonau/Homework9.java
+++ homework9/src/by/antonau/Homework9.java
@@ -1,14 +1,24 @@
 package by.antonau;
 
-//public class Homework9 {
+public class Homework9 {
 
-   // public static void main(String[] args) {
-     //   int x = (int) (Math.random() * 20);
-     //   while (x != 0) {
-     //       x = (int) (Math.random() * 20);
-     //       System.out.print(" " + x + " ");
+    public static void main(String[] args) {
+        int numbers[] = new int[20];
+        int MaxNumber = numbers[0];
+        for (int i = 0; i < 20; i++) {
+            numbers[i] = (int) (Math.random() * 20);
+            System.out.print(" " + numbers[i] + " ");
+        }
+            for(int i = 0; i< numbers.length; i++)
+                if(MaxNumber < numbers[i])
+                MaxNumber = numbers[i];
+        System.out.println(" ");
+            System.out.println("Максимальное число "+ MaxNumber +" ");
+        }
+    }
 
-     //   }
-   //     System.out.println(Math.max (x));
-  //  }
-//}
+
+
+
+
+
