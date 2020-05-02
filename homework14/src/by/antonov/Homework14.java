ebc0fc9d89e1da553add3fd66d7ba8fcdc245003
KirylLog <kirik.by.ru@gmail.com>
2020-05-02 23:31:16 +0300

    new one commit

---
+++ homework14/src/by/antonov/Homework14.java
@@ -0,0 +1,24 @@
+package by.antonov;
+
+public class Homework14 {
+
+    public static void main(String[] args) {
+        int marks[] = new int[5]; //массив, количество учеников
+        for (int i = 0; i < 5; i++) {
+            marks[i] = (int) (Math.random() * 90 + 1); //присваивание оценок
+        }
+        int maxMarkIndex = 0;
+        int maxMark = marks[0]; // присваивание максимального значения оценки
+        for (int i = 0; i < marks.length; i++) { //цикл поиска максимального значения
+            if (maxMark < marks[i]){
+                maxMark = marks[i];
+                maxMarkIndex = i;
+            }
+        }
+        System.out.println("Максимальная оценка - " + maxMark); //Вывод максимальной оценки
+        System.out.println("Её номер - " + maxMarkIndex); //Вывод индекса максимальной оценки
+    }
+}
+
+
+
